7fb16b8bcf0a6d1884c6b67ec6115d65ef980309
LoveDonut <41279677+LoveDonut@users.noreply.github.com>
2024-03-27 15:15:38 +0900

    Update 가장 긴 증가하는 부분 수열 2.cc

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -11,7 +11,6 @@ int main()
     vector<int> numlist;
     cin >> N;
 
-    int maxLength = 0, maxValue = 0, prevValue = 0, num, curLength = 0;
     for(int i=0; i<N; i++)
     {
         cin >> num;
