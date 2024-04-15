c90895558ec910d5b9b884064cbc6c5538efa36f
LoveDonut <41279677+LoveDonut@users.noreply.github.com>
2024-04-15 14:31:48 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
@@ -2,97 +2,82 @@
 
 using namespace std;
 
-vector<vector<int>> RotateKey(vector<vector<int>> key)
+void RotateKey(vector<vector<int>>& key)
 {
-    vector<vector<int>> rotatedKey(key[0].size(), vector<int>(key[0].size()));
-    
     int m = key[0].size();
-    
+    vector<vector<int>> tmp = key;
     for(int i=0; i<m; i++)
     {
         for(int j=0; j<m; j++)
         {
-            rotatedKey[i][j] = key[m-1-j][i];
+            key[i][j] = tmp[m-1-j][i];
         }
     }
-    
-    return rotatedKey;
 }
 
-bool IsOpen(vector<vector<int>> key, vector<vector<int>> lock, int holeNum)
+bool IsOpen(vector<vector<int>> key, vector<vector<int>> lock, int holeCnt)
 {
     int m = key[0].size(), n = lock[0].size();
-    
-    // 열쇠를 자물쇠 왼쪽위부터, 오른쪽아래순으로 이동시킨다.
-    for(int ly= 1-m; ly<n; ly++)
+    for(int ly=1-m;ly<n;ly++)
     {
-        for(int lx= 1-m; lx<n; lx++)
+        for(int lx=1-m;lx<n;lx++)
         {
-            int cnt = holeNum;
-            bool isEnter = true;
-            
-            // 열쇠를 꽂아본다.
-            for(int ky=0; ky<m; ky++)
+            bool isOpen = true;
+            int cnt = 0;
+            for(int ky = 0; ky < m; ky++)
             {
-                for(int kx=0; kx<m; kx++)
+                for(int kx = 0; kx < m; kx++)
                 {
-                    // 자물쇠의 범위를 벗어나는 영역이면 확인하지 않는다.
-                    if(ly + ky < 0 || lx + kx < 0 || ly + ky >= n || lx + kx >= n) continue;
-                    if((lock[ly+ky][lx+kx] + key[ky][kx]) != 1)
+                    if(ly+ky < 0 || lx + kx < 0 || ly + ky >= n || lx + kx >= n) continue;
+                    if(key[ky][kx] == lock[ly+ky][lx+kx])
                     {
-                        isEnter = false;
+                        isOpen = false;
                         break;
                     }
-                    if(lock[ly+ky][lx+kx] == 0 && key[ky][kx] == 1) 
+                    else if(!lock[ly+ky][lx+kx])
                     {
-                        cnt--;
+                        cnt++;
                     }
                 }
-                if(!isEnter)
+                if(!isOpen)
                 {
                     break;
                 }
             }
-            
-            // 열쇠가 꽂히면 문이 열린다.
-            if(!cnt && isEnter)
+            if(isOpen && cnt == holeCnt)
             {
                 return true;
             }
         }
     }
+    
     return false;
-}
-
-// 홈의 개수를 세서 반환하는 함수
-int CountHole(vector<vector<int>> lock)
-{
-    int cnt = 0;
-    for(vector<int> v : lock)
-    {
-        for(int i : v)
-        {
-            if(!i)
-            {
-                cnt++;
-            }
-        }
-    }
-    return cnt;
 }
 
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
     bool answer = false;
-    int holeNum = CountHole(lock);
-    for(int i=0; i<4; i++)
+    int holeCnt = 0, n = lock[0].size();
+    
+    for(int i=0; i<n; i++)
     {
-        if(IsOpen(key, lock, holeNum))
+        for(int j=0; j<n; j++)
+        {
+            if(!lock[i][j])
+            {
+                holeCnt++;
+            }
+        }
+    }
+    
+    for(int i=0; i<4; i++)
+    {        
+        if(IsOpen(key,lock, holeCnt))
         {
             answer = true;
             break;
         }
-        key = RotateKey(key);
+
+        RotateKey(key);
     }
-    
     return answer;
 }
